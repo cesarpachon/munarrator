a49430e28a1e458c3e7ab3148c2a47eaffe790f5
cesarpachon <cesarpachon@gmail.com>
2014-09-14 22:26:17 -0500

    adding narrator_spec.js

---
+++ specs/narrator_spec.js
@@ -0,0 +1,31 @@
+describe("narrator", function(){
+'use strict';
+
+  it("should link action with stage when execute is called", function(){
+    MuNarrator.clean();
+    var counter = 0;
+    MuNarrator.addAction("action1",
+                         MuNarrator.Microaction.newSingleStep("action1",
+                          function(){
+                            counter++;
+                          }));
+
+
+    var stage1 = function(msg_type, params){
+      if(msg_type === "enter"){
+        MuNarrator.execute("action1");
+      }
+    };
+
+    MuNarrator.addStage("stage1", stage1);
+
+    MuNarrator.setActiveStage("stage1");
+
+    MuNarrator.update();
+    expect(counter).toBe(0);
+
+    MuNarrator.update();
+    expect(counter).toBe(1);
+  });
+
+});
